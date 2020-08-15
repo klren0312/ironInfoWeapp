85e7a49701d0b63c1a462ceb657567cf4b0cab78
zzes <kuanglong0312@sina.com>
2020-08-15 09:03:53 +0800

    fix: 结构升级 冗余文件清除

--- ApiServer/app/router/api/upload.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = app => {
-  const { controller, middleware, apiV1Router } = app
-  // upload file
-  apiV1Router.post('/upload', middleware.log, app.jwt, controller.api.upload.index)
-}
